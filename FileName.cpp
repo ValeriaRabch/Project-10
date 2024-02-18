1621a2b6c005e6c0fa17415b9cd6a6f24c8a70cd
ValeriaRabch <rabchenukv@gmail.com>
2024-02-18 19:32:39 +0200

    Add struct Book

---
+++ FileName.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+struct Book {
+	char name[50];
+	char author[20];
+	char publishing[20];
+	char genre[10];
+};
+
+int main() {
+
+	return 0;
+}
