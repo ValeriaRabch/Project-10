fc7c3758f3ab8cb5bfcb82fb39b461f50c5ef1c3
ValeriaRabch <rabchenukv@gmail.com>
2024-02-25 12:24:55 +0200

    Add func Fulling

--- FileName.cpp
+++ FileName.cpp
@@ -10,7 +10,21 @@ struct Book {
 	char genre[10];
 };
 
+void Fulling(Book* l) {
+	for (int i = 0; i < 5; i++) {
+		cout << "Enter name -";
+		cin.getline(l[i].name, 50);
+		cout << "Enter author -";
+		cin.getline(l[i].author, 20);
+		cout << "Enter publishing house -";
+		cin.getline(l[i].publishing, 20);
+		cout << "Enter genre -";
+		cin.getline(l[i].genre, 10);
+	}
+}
+
 int main() {
 	Book library[5];
+	Fulling(library);
 
 }
